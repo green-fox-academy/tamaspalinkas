dfb375bb4c70315a41069a9b449c4132467e23a1
Tamas Palinkas <tamaspalinkas1@gmail.com>
2018-11-02 15:57:43 +0100

    Simplify DrawPyramid

--- week-02/day-1/src/DrawPyramid.java
+++ week-02/day-1/src/DrawPyramid.java
@@ -13,21 +13,16 @@ public class DrawPyramid {
     // The pyramid should have as many lines as the number was
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter a number.");
-        int num = sc.nextInt();
+        int lines = sc.nextInt();
 
-        for (int i=0; i<num; i++) {
-            for (int j=num; j>i; j--) {
+        for (int i = 0; i < lines; i++) {
+            for (int j = lines; j > i; j--) {
                 System.out.print(" ");
             }
-            for (int k=0; k<=i; k++) {
-                System.out.print("*");
-            }
-            for (int l=0; l<i; l++) {
+            for (int k = 0; k <= i * 2; k++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-
     }
 }
