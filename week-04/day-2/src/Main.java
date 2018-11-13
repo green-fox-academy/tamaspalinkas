0b2decbdd07ac75a30be3fcf4f57affe60c8bf16
Tamas Palinkas <tamaspalinkas@admins-MacBook-Pro.local>
2018-11-13 10:47:47 +0100

    Add BlogPost

--- week-04/day-2/src/Main.java
+++ week-04/day-2/src/Main.java
@@ -14,5 +14,23 @@ public class Main {
         superb.bgColor = "Yellow";
         superb.textColor = "Green";
         superb.text = "Superb!";
+
+        BlogPost loremIpsum = new BlogPost();
+        loremIpsum.title = "Lorem Ipsum";
+        loremIpsum.authorName = "John Doe";
+        loremIpsum.publicationDate = "2000.05.04";
+        loremIpsum.text = "Lorem ipsum dolor sit amet.";
+
+        BlogPost waitButWhy = new BlogPost();
+        waitButWhy.title = "Wait but why";
+        waitButWhy.authorName = "Tim Urban";
+        waitButWhy.publicationDate = "2010.10.10";
+        waitButWhy.text = "A popular long-form, stick-figure-illustrated blog about almost everything.";
+
+        BlogPost oneEngineer = new BlogPost();
+        oneEngineer.title = "One Engineer Is Trying to Get IBM to Reckon With Trump";
+        oneEngineer.authorName = "William Turton";
+        oneEngineer.publicationDate = "2017.03.28";
+        oneEngineer.text = "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.";
     }
 }
