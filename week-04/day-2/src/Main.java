c7b5bc3e4a472c17a8a837c2e9039f381d737e15
Tamas Palinkas <tamaspalinkas@admins-MacBook-Pro.local>
2018-11-13 10:33:15 +0100

    Add week-04/day-2 PostIt and Main

---
+++ week-04/day-2/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        PostIt idea1 = new PostIt();
+        idea1.bgColor = "Orange";
+        idea1.textColor = "Blue";
+        idea1.text = "Idea 1";
+
+        PostIt awesome = new PostIt();
+        awesome.bgColor = "Pink";
+        awesome.textColor = "Black";
+        awesome.text = "Awesome";
+
+        PostIt superb = new PostIt();
+        superb.bgColor = "Yellow";
+        superb.textColor = "Green";
+        superb.text = "Superb!";
+    }
+}
---
+++ week-04/day-2/src/PostIt.java
@@ -0,0 +1,5 @@
+public class PostIt {
+    String bgColor;
+    String text;
+    String textColor;
+}
