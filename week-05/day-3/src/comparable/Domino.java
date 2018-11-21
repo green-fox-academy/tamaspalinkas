fe7396e0099646080c5d538d51576d658ee9bdfc
Tamas Palinkas <tamaspalinkas1@gmail.com>
2018-11-21 14:08:27 +0100

    Add Comparable solution

---
+++ week-05/day-3/src/comparable/Domino.java
@@ -0,0 +1,34 @@
+package comparable;
+
+public class Domino implements Comparable {
+  private final int left;
+  private final int right;
+
+  public Domino(int left, int right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  public int getLeftSide() {
+    return left;
+  }
+
+  public int getRightSide() {
+    return right;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + left + ", " + right + "]";
+  }
+
+  @Override
+  public int compareTo(Object o) {
+    Domino compareDomino = (Domino) o;
+    if (this.getLeftSide() < compareDomino.getLeftSide()) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+}
