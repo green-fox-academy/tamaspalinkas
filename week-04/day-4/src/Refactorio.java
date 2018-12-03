f5b789084185efc822075f3ce8c54be0240eb873
Tamas Palinkas <tamaspalinkas1@gmail.com>
2018-12-03 15:32:47 +0100

    Fix factorial for 0

--- week-04/day-4/src/Refactorio.java
+++ week-04/day-4/src/Refactorio.java
@@ -1,14 +1,17 @@
 public class Refactorio {
-    // Create a recursive function called `refactorio`
-    // that returns it's input's factorial
-    public static void main(String[] args) {
-        System.out.println(factorial(10));
-    }
+  // Create a recursive function called `refactorio`
+  // that returns it's input's factorial
+  public static void main(String[] args) {
+    System.out.println(factorial(0));
+  }
 
-    public static int factorial(int n) {
-        if (n == 1) {
-            return 1;
-        }
-        return n * factorial(n - 1);
+  public static int factorial(int n) {
+    if (n == 0) {
+      return 1;
     }
+    if (n == 1) {
+      return 1;
+    }
+    return n * factorial(n - 1);
+  }
 }
