ce783120d8672b4627f238801dd6a24da6331bc2
Tamas Palinkas <tamaspalinkas1@gmail.com>
2018-12-05 13:24:15 +0100

    Add HelloWorld Spring project

---
+++ week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greetings.java
@@ -0,0 +1,27 @@
+package com.greenfoxacademy.springstart.controllers;
+
+public class Greetings {
+  long id;
+  String content;
+
+  public Greetings(long id, String content) {
+    this.id = id;
+    this.content = content;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+}
