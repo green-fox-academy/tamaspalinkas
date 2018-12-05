962f5d0559abd5c0b804c080319bce3c8c382771
Tamas Palinkas <tamaspalinkas1@gmail.com>
2018-12-05 13:36:11 +0100

    Output greets user with input param given

--- week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,14 +1,15 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
   @RequestMapping
-  public Greeting greeting() {
-    Greeting greeting = new Greeting(1,"Hello World!");
+  public Greeting greeting(@RequestParam String p) {
+    Greeting greeting = new Greeting(1,"Hello " + p + "!");
     return greeting;
   }
 }
