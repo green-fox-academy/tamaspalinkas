799065795bd457aa16c8f39c6299bb80933c380f
Tamas Palinkas <tamaspalinkas1@gmail.com>
2018-12-06 10:27:18 +0100

    Add random greetings, random coloring and font size

--- week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/Greeting.java
+++ week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/Greeting.java
@@ -7,6 +7,11 @@ public class Greeting {
   long id;
   String content;
 
+  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+          "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+          "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+          "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
   public Greeting(String content) {
     this.id = atomicLong.incrementAndGet();
 
@@ -27,5 +32,10 @@ public class Greeting {
 
   public void setContent(String content) {
     this.content = content;
+  }
+
+  public String getRandomGreeting() {
+    int rand = (int) (Math.random() * hellos.length);
+    return hellos[rand];
   }
 }
--- week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -1,23 +1,39 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import com.greenfoxacademy.springstart.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.Random;
+
 
 @Controller
 public class HelloWebController {
-  static AtomicLong atomicLong = new AtomicLong(0);
-  long cnt;
 
   @RequestMapping("/web/greeting")
   public String greeting(Model model, @RequestParam String p) {
     model.addAttribute("name", p);
-    model.addAttribute("counter", cnt);
-    cnt = atomicLong.incrementAndGet();
+
+    Greeting greeting = new Greeting(p);
+    model.addAttribute("counter", greeting.getId());
+    model.addAttribute("randomGreeting", greeting.getRandomGreeting());
+    model.addAttribute("style", randomColor() + randomSize());
     return "greeting";
   }
 
+  public String randomSize() {
+    int rand = (int) (Math.random() * 50) + 10;
+    return String.format("; font-size:%dpx", rand);
+  }
+
+  public String randomColor() {
+    Random random = new Random();
+    int r = random.nextInt(255);
+    int g = random.nextInt(255);
+    int b = random.nextInt(255);
+
+    return String.format("color:rgb(%d,%d,%d);", r, g, b);
+  }
 }
