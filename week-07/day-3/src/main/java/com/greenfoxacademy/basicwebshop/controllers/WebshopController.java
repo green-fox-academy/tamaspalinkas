eff32231c9c2cb462b12b4789220689eb8f45deb
Tamas Palinkas <tamaspalinkas1@gmail.com>
2018-12-07 16:45:32 +0100

    Add average stock endpoint

--- week-07/day-3/src/main/java/com/greenfoxacademy/basicwebshop/controllers/WebshopController.java
+++ week-07/day-3/src/main/java/com/greenfoxacademy/basicwebshop/controllers/WebshopController.java
@@ -25,8 +25,9 @@ public class WebshopController {
     webShop.addItem(new ShopItem("Coca cola", "0.5l standard coke", 25.0, 0));
     webShop.addItem(new ShopItem("Wokin", "Chicken with fried ricend WOKIN sauce", 119.0, 100));
     webShop.addItem(new ShopItem("T-shirt", "Blue with a corgi on a bike", 300.0, 1));
+    webShop.addItem(new ShopItem("Running shoes", "Nike running shoes", 1000.0, 5));
 
-    webShop.addItem(new ShopItem("T-shirt", "Blue with a corgi on a bike", 300888.0, 0));
+    //webShop.addItem(new ShopItem("T-shirt", "Blue with a corgi on a bike", 300888.0, 0));
   }
 
 
@@ -36,20 +37,18 @@ public class WebshopController {
     return "index";
   }
 
-  @GetMapping("/available")
+  @GetMapping("/only-available")
   public String available(Model model) {
 
-
-    Stream<ShopItem> availableShopItems = webShop.getShopItems()
+    List<ShopItem> availableItems = webShop.getShopItems()
             .stream()
-            .filter(s -> s.getQuantity() > 0);
-    List<ShopItem> availableItems = availableShopItems.collect(Collectors.toList());
-
+            .filter(s -> s.getQuantity() > 0)
+            .collect(Collectors.toList());
     model.addAttribute("shopItems", availableItems);
     return "index";
   }
 
-  @RequestMapping("/most-expensive-available")
+  @GetMapping("/most-expensive-available")
   public String mostExpensive(Model model) {
     ShopItem minPricedItem = webShop.getShopItems()
             .stream()
@@ -60,4 +59,34 @@ public class WebshopController {
     model.addAttribute("shopItems", minPricedItem);
     return "index";
   }
+
+  @GetMapping("/cheapest-first")
+  public String cheapestFirst(Model model) {
+    List<ShopItem> ascendingPriceList = webShop.getShopItems()
+            .stream()
+            .sorted(Comparator.comparing(ShopItem::getPrice))
+            .collect(Collectors.toList());
+    model.addAttribute("shopItems", ascendingPriceList);
+    return "index";
+  }
+
+//  @GetMapping("/nike")
+//  public String containsNike(Model model) {
+//    boolean hasNike = webShop.getShopItems()
+//            .stream()
+//            .anyMatch(o -> o.getName().contains("nike"));
+//    if (hasNike) {
+//      model.addAttribute()
+//    }
+//  }
+
+  @GetMapping("/average-stock")
+  public String averageStock(Model model) {
+    double totalStock = webShop.getShopItems()
+            .stream()
+            .mapToDouble(o -> o.getQuantity()).sum();
+    model.addAttribute("average", "Average Stock:" + totalStock/webShop.getShopItems().size());
+    return "index";
+  }
+
 }
